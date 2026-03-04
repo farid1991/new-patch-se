adbc56081d6781b05b9c45499ed3fa7076802cbf
farid1991 <farid.irwan.1991@gmail.com>
2026-03-04 21:55:00 +0700

    db_pause: add J108_R7EA011 port

---
+++ db_pause/target/J108_R7EA011.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define DB3350_R2
+
+#define ADDR_GUIObject_SoftKeys_SetVisible (0x142CE958 | 1)
+#define ADDR_BrowserItem_Get_SUB_EXECUTE (0x144CFF08 | 1)
+
+#define ACTION_PAUSE 0x6F
+#define ACTION_PLAY 0xFF
