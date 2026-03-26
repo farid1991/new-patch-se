8c06b8dc02c2f703a6fa1f8823c3d0770c915251
Farid Irwan <farid.irwan.1991@gmail.com>
2026-03-27 01:57:24 +0700

    refactor: replace per-file device ifdefs with centralized device.h

--- png_info/src/patch.c
+++ png_info/src/patch.c
@@ -1,14 +1,4 @@
-#if defined(C510_R1HA035)
-#include <C510_R1HA035.h>
-#elif defined(J108_R7EA011)
-#include <J108_R7EA011.h>
-#elif defined(K600_R2BB001)
-#include <K600_R2BB001.h>
-#elif defined(W760_R3EM001)
-#include <W760_R3EM001.h>
-#elif defined(W810_R4EA031)
-#include <W810_R4EA031.h>
-#endif
+#include "device.h"
 
 #define USE_LIBPNG
 #include <libse.h>
